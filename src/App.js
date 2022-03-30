68388e17b084b6c2bf2b306301f0095b9bd62109
amyloudav <amyloudav@gmail.com>
2022-03-30 17:44:42 +0200

    trending done

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
       <div className="app">
         <Container>
           <Routes>
-            <Route path='/' component={Trending} exact />
-            <Route path='/movies' component={Movies} />
-            <Route path='/series' component={Series} />
-            <Route path='/search' component={Search} />
+            <Route path='/' element={ <Trending/> } exact />
+            <Route path='/movies' element={Movies} />
+            <Route path='/series' element={Series} />
+            <Route path='/search' element={Search} />
           </Routes>
         </Container>
       </div>
--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Trending = () => {
+  const [content, setContent] = useState([]);
+
+  const FetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/week?api_key${process.env.REACT_APP_API_KEY}`
+    );
+
+    console.log(data);
+
+    setContent(data.results);
+  };
+
+    useEffect(() => {
+      FetchTrending();
+    }, [])
+
   return (
     <div>
-        <span className='pageTitle'>Trending</span>
+      <span className="pageTitle">Trending</span>
+      <div className="trending"></div>
     </div>
-  )
-}
+  );
+};
 
 export default Trending;
