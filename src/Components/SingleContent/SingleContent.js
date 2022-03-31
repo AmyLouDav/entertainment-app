005389ea21f7ffcf0c38bb928784bd5166432652
amyloudav <amyloudav@gmail.com>
2022-03-31 14:37:09 +0200

    pagination done

--- src/Components/SingleContent/SingleContent.js
+++ src/Components/SingleContent/SingleContent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { img_300, unavailable } from "../../config/config";
 import "./SingleContent.css";
-import { Badge } from "@mui/material"
+import { Badge } from "@mui/material";
 
 const SingleContent = ({
   id,
@@ -13,11 +13,19 @@ const SingleContent = ({
 }) => {
   return (
     <div className="media">
-        <Badge badgeContent={vote_average} color={vote_average > 6 ? "primary" : "secondary" }/>
-      <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
+      <Badge
+        badgeContent={vote_average}
+        color={vote_average > 7 ? "primary" : "secondary"}
+      />
+      <img
+        className="poster"
+        src={poster ? `${img_300}/${poster}` : unavailable}
+        alt={title}
+      />
       <b className="title">{title}</b>
-      <span className="subTitle">{media_type === "tv" ? "TV Series" : "Movie"}
-      <span className="subTitle">{date}</span>
+      <span className="subTitle">
+        {media_type === "tv" ? "TV Series" : "Movie"}
+        <span className="subTitle">{date}</span>
       </span>
     </div>
   );
--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import CustomPagination from "../../Components/Pagination/CustomPagination";
 import SingleContent from "../../Components/SingleContent/SingleContent";
 import './Trending.css';
 
 const Trending = () => {
+  const [page, setPage] = useState(1)
   const [content, setContent] = useState([]);
 
   const fetchTrending = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
+      `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
     );
 
     setContent(data.results);
@@ -16,7 +18,7 @@ const Trending = () => {
 
   useEffect(() => {
     fetchTrending();
-  }, []);
+  }, [page]);
 
   return (
     <div>
@@ -35,6 +37,7 @@ const Trending = () => {
             />
           ))}
       </div>
+      <CustomPagination setPage={setPage} />
     </div>
   );
 };
