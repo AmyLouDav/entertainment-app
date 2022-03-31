ea1bbcedfc1106ec47e01e081b209e9aae25bbf8
amyloudav <amyloudav@gmail.com>
2022-03-31 15:00:00 +0200

    dark theme added

--- src/Components/MainNav.js
+++ src/Components/MainNav.js
@@ -1,33 +1,35 @@
-import * as React from 'react';
-import { useEffect } from 'react';
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import MovieIcon from '@mui/icons-material/Movie';
-import SearchIcon from '@mui/icons-material/Search';
-import TvIcon from '@mui/icons-material/Tv';
-import { useNavigate } from 'react-router-dom'
+import * as React from "react";
+import { useEffect } from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import MovieIcon from "@mui/icons-material/Movie";
+import SearchIcon from "@mui/icons-material/Search";
+import TvIcon from "@mui/icons-material/Tv";
+import { useNavigate } from "react-router-dom";
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
   const history = useNavigate();
 
   useEffect(() => {
-      if (value === 0) history("/");
-      else if (value === 1) history("/movies")
-      else if (value === 2) history("/series")
-      else if (value === 3) history("/search")
+    if (value === 0) history("/");
+    else if (value === 1) history("/movies");
+    else if (value === 2) history("/series");
+    else if (value === 3) history("/search");
   }, [value, history]);
 
   return (
-    <Box sx={{ width: '100%',
-        position: 'fixed',
+    <Box
+      sx={{
+        width: "100%",
+        position: "fixed",
         bottom: 0,
-        backgroundColor: '#2d313a',
+        backgroundColor: "#2d313a",
         zIndex: 100,
-     }
-     }>
+      }}
+    >
       <BottomNavigation
         showLabels
         value={value}
--- src/Components/Pagination/CustomPagination.js
+++ src/Components/Pagination/CustomPagination.js
@@ -1,17 +1,30 @@
-import { SecurityUpdateGoodRounded } from "@mui/icons-material"
-import { Pagination } from "@mui/material"
+import { Pagination } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
+import "../Pagination/CustomPagination.css";
+import { dark, light } from "@mui/material/styles/createPalette";
+
+const darkTheme = createTheme({
+  palette: {
+    type: light,
+  },
+});
 
 const CustomPagination = ({ setPage, numOfPages = 10 }) => {
-    const handlePageChange = (page) => {
-        setPage(page);
-        window.scroll(0, 0)
-    };
+  const handlePageChange = (page) => {
+    setPage(page);
+    window.scroll(0, 0);
+  };
 
   return (
-    <div>
-    <Pagination count={numOfPages} onChange={(e) => handlePageChange(e.target.textContent)} />
+    <div className="pagination">
+      <ThemeProvider theme={darkTheme}>
+        <Pagination
+          count={numOfPages}
+          onChange={(e) => handlePageChange(e.target.textContent)}
+        />
+      </ThemeProvider>
     </div>
-  )
+  );
 };
 
-export default CustomPagination
+export default CustomPagination;
