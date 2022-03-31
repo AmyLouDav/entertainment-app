801e8bfdd9da96ece15e7d27941b0ef1152c42b2
amyloudav <amyloudav@gmail.com>
2022-03-31 09:57:12 +0200

    connection made to the API

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -4,9 +4,9 @@ import React, { useEffect, useState } from "react";
 const Trending = () => {
   const [content, setContent] = useState([]);
 
-  const FetchTrending = async () => {
+  const fetchTrending = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/week?api_key${process.env.REACT_APP_API_KEY}`
+      `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
     );
 
     console.log(data);
@@ -15,13 +15,19 @@ const Trending = () => {
   };
 
     useEffect(() => {
-      FetchTrending();
+      fetchTrending();
     }, [])
 
   return (
     <div>
       <span className="pageTitle">Trending</span>
-      <div className="trending"></div>
+      <div className="trending">
+        {
+          content && content.map((c) => {
+            console.log(c)
+          })
+        }
+      </div>
     </div>
   );
 };
